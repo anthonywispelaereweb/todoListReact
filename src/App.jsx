2153c96eaf4787936521e0b3bcef94766efbc7b9
anthony wispelaere <awispelaere@partner.auchan.fr>
2025-11-16 14:54:47 +0100

    add analytics vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import TodoFeature from './components/TodoFeature';
 import TodoProvider from './components/TodoProvider';
-
+import { Analytics } from "@vercel/analytics/next"
 function App() {
   return (
     <TodoProvider>
       <TodoFeature />
+      <Analytics />
     </TodoProvider>
   );
 }
