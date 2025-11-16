9dae7ffe0b83fd566c56e2badac4921331bc9b69
anthony wispelaere <awispelaere@partner.auchan.fr>
2025-11-16 15:01:10 +0100

    add speed-insights vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import TodoFeature from './components/TodoFeature';
 import TodoProvider from './components/TodoProvider';
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 function App() {
   return (
     <TodoProvider>
       <TodoFeature />
       <Analytics />
+      <SpeedInsights />
     </TodoProvider>
   );
 }
